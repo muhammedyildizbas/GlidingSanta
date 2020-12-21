b290951df8172b0d0a5669e2c84c8ee49fd5f746
Burak Kocatürk <59361739+BK-97@users.noreply.github.com>
2020-12-21 16:48:04 +0300

    Files,Managers,Scenes,Singleton,SetSceneLoad

---
+++ Assets/[Game]/GlidingSanta/Scripts/Managers/InitManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class InitManager : MonoBehaviour
+{
+    private void Start()
+    {
+        SceneManager.LoadScene("GlidingSantaUI", LoadSceneMode.Additive);
+        SceneManager.LoadScene("GlidingSantaGame", LoadSceneMode.Additive);
+        Destroy(gameObject);
+    }
+}
