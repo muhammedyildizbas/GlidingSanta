83d604a27a32afce87194ef0271f50a3bc097468
Burak Kocatürk <59361739+BK-97@users.noreply.github.com>
2020-12-25 00:29:26 +0300

    Collectables-Not Finished

---
+++ Assets/[Game]/GlidingSanta/Scripts/Collectables/CollectablesText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectablesText : Singleton<CollectablesText>
+{
+    Text text;
+    Text Text { get { return (text == null) ? text = GetComponent<Text>() : text; } }
+
+
+
+
+    public void UpdateCollectionText(int number)
+    {
+        number += number;
+        Text.text = "Stars: " + number;
+
+    }
+}
