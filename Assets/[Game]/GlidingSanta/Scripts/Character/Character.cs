df4877c54de882d230279fdfa94d7a2785a11b72
muhammedyildiz3465@gmail.com <63956100+muhammedyildizbas@users.noreply.github.com>
2020-12-27 15:36:58 +0300

    MenuLevel2

---
+++ Assets/[Game]/GlidingSanta/Scripts/Character/Character.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    Rigidbody _rigidbody;
+    bool gameStarted=false;
+    public float timer=3f;
+    public Rigidbody Rigidbody { get { return (_rigidbody == null) ? _rigidbody = GetComponent<Rigidbody>() : _rigidbody; } }
+    public float screenLimit=7;
+    Touch touch;
+    private Vector3 moveVector;
+    private Vector3 CurrentAngle;
+    private Vector3 TargetForThrow;
+    public float speed;
+    public float ThrowSpeed = 5f;
+    public float AngleSpeed = 0;
+  
+    private CharacterController controller;
+
+    public GameObject particles; //particle sistem için
+    private void Start()
+    {
+        StartCoroutine(WaitForStartCoroutine());
+
+        controller = GetComponent<CharacterController>();
+        CurrentAngle = transform.eulerAngles;
+        TargetForThrow = new Vector3(0f, 35f, 0f);
+    }
+    private void Update()
+    {
+        ThrowPlayerAtStart();
+        
+        if (!gameStarted)
+            return;
+        Movement();
+        BoostController();
+
+    }
+    public void BoostController()
+    {
+        if (BoostManager.Instance.Boost <= 0)
+            Debug.Log("EndGame");
+        else
+            BoostManager.Instance.Boost -= Time.deltaTime;
+    }
+
+    public void Movement()
+    {
+
+        moveVector.x = Input.GetAxisRaw("Horizontal") * speed;
+        Rigidbody.velocity = (moveVector) * Time.deltaTime * speed;
+
+        if(moveVector.x > 0)
+        {
+            //TargetAngle = new Vector3(transform.rotation.x, BendRight, transform.rotation.z);
+            CurrentAngle = new Vector3(
+                Mathf.LerpAngle(CurrentAngle.x, 0, Time.deltaTime * AngleSpeed),
+                Mathf.LerpAngle(CurrentAngle.y, 0f, Time.deltaTime * AngleSpeed),
+                Mathf.LerpAngle(CurrentAngle.z, -40f, Time.deltaTime * AngleSpeed)
+                );
+            transform.eulerAngles = CurrentAngle;
+
+        }
+        if(moveVector.x < 0)
+        {
+            //TargetAngle = new Vector3(transform.rotation.x, BendLeft, transform.rotation.z);
+            CurrentAngle = new Vector3(
+                Mathf.LerpAngle(CurrentAngle.x, 0, Time.deltaTime * AngleSpeed),
+                Mathf.LerpAngle(CurrentAngle.y, 0, Time.deltaTime * AngleSpeed),
+                Mathf.LerpAngle(CurrentAngle.z, 40f, Time.deltaTime * AngleSpeed)
+                );
+
+            transform.eulerAngles = CurrentAngle;
+
+        }
+        #region Mobile
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Moved)
+            {
+                transform.position += Vector3.right * (6 * touch.deltaPosition.x);
+                if (transform.position.x >= screenLimit)
+                    transform.position = new Vector3(screenLimit, transform.position.y, transform.position.z);
+                if (transform.position.x <= -screenLimit)
+                    transform.position = new Vector3(-screenLimit, transform.position.y, transform.position.z);
+            }
+        }
+        #endregion
+    }
+
+    public void ThrowPlayerAtStart()
+    {
+        transform.position = Vector3.Lerp(transform.position, TargetForThrow, ThrowSpeed * Time.deltaTime);
+    }
+
+    IEnumerator WaitForStartCoroutine()
+    {
+        yield return new WaitForSeconds(timer);
+        gameStarted = true;
+    }
+
+    #region Collectable Trigger
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ICollectable collectedObj = other.GetComponent<ICollectable>();
+
+        if (collectedObj != null)
+        {
+            GameObject particle = Instantiate(particles, transform.position + new Vector3(0,0,5),Quaternion.identity);
+            particle.GetComponent<ParticleSystem>().Play();
+
+            collectedObj.CollectAndText();
+            Destroy(particle.gameObject,1);
+        }
+
+        
+
+    }
+
+    #endregion
+
+}//gg ez
---
+++ Assets/[Game]/GlidingSanta/Scripts/PoolSystem/TileMovementController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMovementController : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z -TileManager.Instance.tileSpeed*Time.deltaTime);
+    }
+}
